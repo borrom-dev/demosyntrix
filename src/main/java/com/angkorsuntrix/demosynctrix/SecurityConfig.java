aa582b5fc3ea9ba375339ade83d1da952c48e446
Borrom Dev <borrom.dev@gmail.com>
2019-09-01 11:09:44 +0700

    ch-05: user registeration

--- src/main/java/com/angkorsuntrix/demosynctrix/SecurityConfig.java
+++ src/main/java/com/angkorsuntrix/demosynctrix/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.angkorsuntrix.demosynctrix;
 
+import com.angkorsuntrix.demosynctrix.repository.UserRepository;
 import com.angkorsuntrix.demosynctrix.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -22,13 +24,19 @@ import java.util.Collections;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilterBefore(
+                        new RegisterFilter("/register", authenticationManager(), userRepository, new BCryptPasswordEncoder()),
+                        UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(
                         new LoginFilter("/login", authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class)
