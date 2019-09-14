4122cdd0004b6d27137ec11b5214d59338870c5f
Borrom Sync <borrom.dev@gmail.com>
2019-09-14 16:07:35 +0700

    ch-08: clean up page

--- src/main/java/com/angkorsuntrix/demosynctrix/DemoSyncTrixApplication.java
+++ src/main/java/com/angkorsuntrix/demosynctrix/DemoSyncTrixApplication.java
@@ -28,10 +28,10 @@ public class DemoSyncTrixApplication {
             public void run(String... args) throws Exception {
                 userRepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
                 userRepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
-                pageRepository.save(new Page("/", "", 0, 0, 0, 0, "page", "home"));
-                pageRepository.save(new Page("/android", "", 0, 0, 0, 0, "page", "Android"));
-                pageRepository.save(new Page("/kotlin", "", 0, 0, 0, 0, "page", "Kotlin"));
-                pageRepository.save(new Page("/java", "", 0, 0, 0, 0, "page", "Java"));
+                pageRepository.save(new Page("/", "",   "page", "home"));
+                pageRepository.save(new Page("/android", "",     "page", "Android"));
+                pageRepository.save(new Page("/kotlin", "",  "page", "Kotlin"));
+                pageRepository.save(new Page("/java", "",  "page", "Java"));
             }
         };
     }
--- src/main/java/com/angkorsuntrix/demosynctrix/domain/Post.java
+++ src/main/java/com/angkorsuntrix/demosynctrix/domain/Post.java
@@ -1,9 +1,8 @@
 package com.angkorsuntrix.demosynctrix.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity(name = "posts")
@@ -15,12 +14,18 @@ public class Post {
     private String title;
     private String slug;
     private String body;
+    private boolean published;
     private Date publish_at;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "page_id", nullable = false)
+    @JsonIgnore
+    private Page page;
 
-    public Post(String title, String slug, String body, Date publish_at) {
+    public Post(String title, String slug, String body, boolean published, Date publish_at) {
         this.title = title;
         this.slug = slug;
         this.body = body;
+        this.published = published;
         this.publish_at = publish_at;
     }
 
@@ -54,6 +59,14 @@ public class Post {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public void setPublished(boolean published) {
+        this.published = published;
     }
 
     public Date getPublish_at() {
