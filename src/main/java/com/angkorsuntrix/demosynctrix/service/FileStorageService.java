c690dfd3bb28123e10331097572ad834a4deaff5
Borrom Sync <borrom.dev@gmail.com>
2019-09-26 21:39:34 +0700

    ch-15: file upload

---
+++ src/main/java/com/angkorsuntrix/demosynctrix/service/FileStorageService.java
@@ -0,0 +1,60 @@
+package com.angkorsuntrix.demosynctrix.service;
+
+import com.angkorsuntrix.demosynctrix.exception.FileStorageException;
+import com.angkorsuntrix.demosynctrix.property.FileStorageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.*;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageProperties properties) {
+        fileStorageLocation = Paths.get(properties.getUploadDir())
+                .toAbsolutePath().normalize();
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where the uploaded", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! filename contain invalid path");
+            }
+            Path targetLocation = fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (IOException e) {
+            throw new FileStorageException("Could not store file: " + fileName);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new FileStorageException("File not found: " + fileName);
+            }
+        } catch (MalformedURLException e) {
+            throw new FileStorageException("File not found: " + fileName, e);
+        }
+    }
+}
